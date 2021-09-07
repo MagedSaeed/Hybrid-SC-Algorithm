9a60441e5986ae388f29d0739f1b560a7756001f
MagedSaeed <mageedsaeed1@gmail.com>
2021-09-07 19:27:21 +0300

    finish initial draft of get_initial_solution function

--- code/data_classes.py
+++ code/data_classes.py
@@ -1,10 +1,37 @@
 class Facility:
-    def __init__(self, establishment_cost=0, capacity=0, transportation_cost=dict()):
+    def __init__(
+        self,
+        establishment_cost=0,
+        capacity=0,
+        transportation_cost=dict(),
+    ):
         self.establishment_cost = establishment_cost
         self.capacity = capacity
         self.transportation_cost = transportation_cost
+        self.is_open = False
 
 
 class Echelon:
-    def __init__(self, facilities):
+    def __init__(self, facilities, demand):
         self.facilities = facilities
+        self.demand = demand
+
+    def greedy_sort(self, other_echelon):
+        """
+        This function will return the echelon facilities sorted based on the greedy formula proposed in the greedy initial solution,
+        """
+        sorted_facilities = sorted(
+            self.facilities,
+            key=lambda facility: (facility.establishment_cost / facility.capacity)
+            * sum(
+                [
+                    cost
+                    for other_facility, cost in facility.transportation_cost.items()
+                    if other_facility in other_echelon.facilities
+                ]
+            ),
+        )
+        return sorted_facilities
+
+    def total_capacity(self):
+        return sum(facility.capacity for facility in self.facilities)
--- code/initial_solution.py
+++ code/initial_solution.py
@@ -1,25 +1,48 @@
 import random
 
-from .data_classes import Echelon, Facility
+from data_classes import Echelon, Facility
 
 # set the random seed
 random.seed(10)
 
-suppliers_echelon = Echelon(facilities=list())
-plants_echelon = Echelon(facilities=list())
-warehouses_echelon = Echelon(facilities=list())
-distribution_centers_echelon = Echelon(facilities=list())
-customers_echelon = Echelon(facilities=list())
+suppliers_echelon = Echelon(
+    facilities=list(),
+    demand=random.randint(100, 1000),
+)
+plants_echelon = Echelon(
+    facilities=list(),
+    demand=random.randint(
+        100,
+        1000,
+    ),
+)
+warehouses_echelon = Echelon(
+    facilities=list(),
+    demand=random.randint(100, 1000),
+)
+distribution_centers_echelon = Echelon(
+    facilities=list(),
+    demand=random.randint(
+        100,
+        1000,
+    ),
+)
+customers_echelon = Echelon(
+    facilities=list(),
+    demand=random.randint(100, 1000),
+)
 
 # let us say that we have 5 facilities for each echelon
 
-for echelon in [
+echelons = [
     suppliers_echelon,
     plants_echelon,
     warehouses_echelon,
     distribution_centers_echelon,
     customers_echelon,
-]:
+]
+
+for echelon in echelons:
     # fill the facilities for each echelon
     for _ in range(5):
         facility = Facility(
@@ -27,9 +50,28 @@ for echelon in [
             capacity=random.randint(100, 1000),
         )
         echelon.facilities.append(facility)
-    # fill the transportation cost between facilities
-    # TODO
+
+for i in range(len(echelons) - 1, 0, -1):
+    current_echelon = echelons[i]
+    prev_echelon = echelons[i - 1]
+    for c_facility in current_echelon.facilities:
+        for p_facility in prev_echelon.facilities:
+            transportation_cost = random.randint(100, 1000)
+            c_facility.transportation_cost[p_facility] = transportation_cost
+            p_facility.transportation_cost[c_facility] = transportation_cost
 
 
 def get_initial_solution():
-    pass
+    for k in range(len(echelons) - 2, 0, -1):
+        current_echelon = echelons[k]
+        next_echelon = echelons[k + 1]
+        current_echelon_open_facilities_capacity = 0
+        for facility in current_echelon.facilities:
+            if current_echelon_open_facilities_capacity < next_echelon.demand:
+                facility.is_open = True
+                current_echelon_open_facilities_capacity += facility.capacity
+            else:
+                break
+
+
+get_initial_solution()
