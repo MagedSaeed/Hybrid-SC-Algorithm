3e32f3cafb4e8f292836146b5d9ce74846080e66
MagedSaeed <msmj2015@gmail.com>
2021-11-14 01:26:57 +0300

    fix bug in getting random params

--- code/data_classes.py
+++ code/data_classes.py
@@ -390,7 +390,7 @@ class MarketFacility:  # Market M
     def get_random_echelon(cls):
         howmany = NUMBER_OF_MARKETS
         number_of_products = NUMBER_OF_PRODUCTS
-        random_products_demand = 60 * np.random.rand(howmany, number_of_products) + 80
+        random_products_demand = 600 * np.random.rand(howmany, number_of_products) + 800
         facilities = list()
         for i in range(howmany):
             facility = cls(
