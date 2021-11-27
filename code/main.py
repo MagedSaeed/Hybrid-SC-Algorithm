d383481778a6a9375368180c2a5b698a5f6476e1
MagedSaeed <msmj2015@gmail.com>
2021-11-27 10:37:00 +0300

    update main file

--- code/main.py
+++ code/main.py
@@ -1,5 +1,13 @@
-from supply_chain_network import SupplyChainNetwork
+# from supply_chain_network import SupplyChainNetwork
 
-network = SupplyChainNetwork.get_random_network()
-network.get_initial_solution()
-network.descripe()
+# network = SupplyChainNetwork.get_random_network()
+# network.get_initial_solution()
+# network.descripe()
+
+
+from data_classes import SupplyChainNetwork
+from lp_model import LPModel
+
+net = SupplyChainNetwork()
+net.apply_initial_greedy_solution()
+model = LPModel(net)
