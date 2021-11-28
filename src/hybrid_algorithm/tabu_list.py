b36a8ed08e5f85bc2c643e3b1a064d615462b555
MagedSaeed <msmj2015@gmail.com>
2021-11-28 14:39:24 +0300

    add tabu list data structure

---
+++ src/hybrid_algorithm/tabu_list.py
@@ -0,0 +1,36 @@
+class TabuList(list):
+
+    # Read-only
+    @property
+    def maxLen(self):
+        return self._maxLen
+
+    def __init__(self, max_length, *args, **kwargs):
+        self._maxLen = max_length
+        super().__init__(self, *args, **kwargs)
+
+    def _truncate(self):
+        """Called by various methods to reinforce the maximum length."""
+        dif = len(self) - self._maxLen
+        if dif > 0:
+            self[:dif] = []
+
+    def append(self, x):
+        list.append(self, x)
+        self._truncate()
+
+    def insert(self, *args):
+        list.insert(self, *args)
+        self._truncate()
+
+    def extend(self, x):
+        list.extend(self, x)
+        self._truncate()
+
+    def __setitem__(self, *args):
+        list.__setitem__(self, *args)
+        self._truncate()
+
+    def __setslice__(self, *args):
+        list.__setslice__(self, *args)
+        self._truncate()
