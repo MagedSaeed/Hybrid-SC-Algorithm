856a95b2a3b5eed9575a3d5557c00a4c4ec7377d
MagedSaeed <msmj2015@gmail.com>
2022-01-02 23:44:34 +0300

    call hybrid algorithm in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from lp_model import LPModel
 from supply_chain_network import SupplyChainNetwork
+from hybrid_algorithm.core import HybridAlgorithm
 
 net = SupplyChainNetwork()
 net.apply_initial_greedy_solution()
 model = LPModel(net)
+model.multi_objective_value
+hyb = HybridAlgorithm(net)
+new_net = hyb.optimize()
