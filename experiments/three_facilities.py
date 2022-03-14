678e96a5c2076f0a8af1c5ffe9ce5d8cbdf89f49
MagedSaeed <msmj2015@gmail.com>
2022-03-14 08:50:03 +0300

    reflect changes in the three_facilities experiment

--- experiments/three_facilities.py
+++ experiments/three_facilities.py
@@ -1,14 +1,33 @@
 from hybrid_algorithm import SupplyChainNetwork, LPModel, HybridAlgorithm
+import random
+import numpy as np
 
+seed = 0
 
-net = SupplyChainNetwork(
-    facilities_count=3,
-    raw_materials_count=4,
-    markets_count=6,
-    products_count=3,
-)
-net.initialize_random_network()
-net.apply_initial_greedy_solution()
-model = LPModel(net)
-hyb = HybridAlgorithm(net)
-hyb.optimize()
+np.random.seed(0)
+random.seed(0)
+
+solutions_list = []
+
+for i in range(1, 1001):
+    print("#" * 100)
+    print(f"optimizing {i} network".center(100, "#"))
+    print("#" * 100)
+    net = SupplyChainNetwork(
+        facilities_count=3,
+        raw_materials_count=4,
+        markets_count=10,
+        products_count=4,
+    )
+    net.initialize_random_network()
+    net.apply_initial_greedy_solution()
+    model = LPModel(net)
+    hyb = HybridAlgorithm(
+        network=net,
+        tabu_size=5,
+        T=50,
+        Tf=1,
+        alpha=0.9,
+        K=0.5 * (3 * 4 + 10),
+    )
+    solutions_list.append(hyb.optimize())
