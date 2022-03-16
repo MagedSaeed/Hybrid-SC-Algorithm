e31e13705ba7693c669719a31704b129a27bd74b
MagedSaeed <msmj2015@gmail.com>
2022-03-16 23:26:56 +0300

    add variables to three_facilities experiments

--- experiments/three_facilities.py
+++ experiments/three_facilities.py
@@ -7,7 +7,16 @@ seed = 0
 np.random.seed(seed)
 random.seed(seed)
 
-N = 3 * 4 + 10
+facilities_count = 3
+markets_count = 10
+products_count = 4
+
+N = facilities_count * 4 + markets_count
+
+max_neighbors = int(
+    (1 - ((markets_count + facilities_count) / (facilities_count * markets_count)))
+    * (facilities_count ** 4)
+)
 
 solutions_list = []
 
