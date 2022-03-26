9395b10e88d656742c805db2d272f6bc7dbe91ef
MagedSaeed <msmj2015@gmail.com>
2022-03-26 18:58:58 +0300

    fix bug in getting config values in base_facilities file.

--- hybrid_algorithm/facilities/base_facility.py
+++ hybrid_algorithm/facilities/base_facility.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass
 from hybrid_algorithm.config import AppConfig
 
-config = AppConfig.config
-facilities_config = config["facilities"]
-
 
 @dataclass
 class BaseFacility:
     @classmethod
     def _configure(cls):
+        config = AppConfig.config
+        facilities_config = config["facilities"]
         cls.NUMBER_OF_SUPPLIERS = int(facilities_config["facilities_count"])
         cls.NUMBER_OF_PLANTS = int(facilities_config["facilities_count"])
         cls.NUMBER_OF_WAREHOUSES = int(facilities_config["facilities_count"])
