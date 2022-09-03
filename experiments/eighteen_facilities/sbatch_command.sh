028e7b3c84023df488adc6a04863d6fca064ece0
MagedSaeed <mageedsaeed1@gmail.com>
2022-09-03 17:36:28 +0300

    update sbatch config

--- experiments/eighteen_facilities/sbatch_command.sh
+++ experiments/eighteen_facilities/sbatch_command.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=eighteen_facilities_exp                            # Job name
 #SBATCH --nodes=3                                                     # Run all processes on a single node	
-#SBATCH --ntasks=243                                                  # Run a single task	
+#SBATCH --ntasks=81                                                  # Run a single task	
 #SBATCH --mem=8gb                                                     # Total memory limit
 #SBATCH --output=/cpgpfs/home/ptashraf/MagedFiles/Hybrid-SC-Algorithm/experiments/eighteen_facilities/log%j.out     # Standard output and error log
 
--- experiments/six_facilities/sbatch_command.sh
+++ experiments/six_facilities/sbatch_command.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=six_facilities_exp                                 # Job name
 #SBATCH --nodes=3                                                     # Run all processes on a single node	
-#SBATCH --ntasks=162                                                  # Run a single task	
+#SBATCH --ntasks=81                                                  # Run a single task	
 #SBATCH --mem=8gb                                                     # Total memory limit
 #SBATCH --output=/cpgpfs/home/ptashraf/MagedFiles/Hybrid-SC-Algorithm/experiments/six_facilities/log%j.out     # Standard output and error log
 
